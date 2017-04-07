7c72cefce6c9f4d749676be694568a61e6e0f620
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-04-06 18:10:37 -0700

    End of day 4 passing all specs

--- Frogger/js/app.js
+++ Frogger/js/app.js
@@ -2,14 +2,35 @@ Object.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
   ctx.fillStyle = 'white' // render score and lives on top of screen
   ctx.clearRect(0,0,1000,50)
-  ctx.font = "20px Verdana"
+  ctx.font = "30px Courier"
   ctx.fillStyle = 'red'
-  ctx.fillText(`Bob's Score: ${this.score}`,350, 45)
-  ctx.fillText(`Lives: ${this.lives}`, 0, 45)
+  ctx.fillText(`Bobs Left: ${this.lives}`, 0, 45)
+  // ctx.fillStyle = 'black'
+  // ctx.fillText(`Level: ${player.level}`, 220, 45)
+  ctx.fillStyle = 'orange'
+  ctx.fillText(`Score: ${this.score}`, 350, 45)
+// Show game over message when no lives left, reload page
+  if (this.lives <= 0) {
+    ctx.font = "100px Courier"
+    ctx.fillStyle = 'red'
+    ctx.fillText(`RIP BOB`,45, 400)
+    setTimeout(function() {
+      window.location.reload()}, 2500)
+    }
+  if (this.lives === 3) {
+    ctx.font = "35px Courier"
+    ctx.fillStyle = 'white'
+    ctx.fillText(`DON\'T LET BOB GET HIT!`, 25, 100)
+  }
+  if (this.lives === 1) {
+    ctx.font = "35px Courier"
+    ctx.fillStyle = 'white'
+    ctx.fillText(`BOB HAS 1 MORE CHANCE!`,25, 100)
+  }
 }
 // Enemies our player must avoid
 class Palette {
-  constructor() {
+  constructor(x, y) {
       this.x = -150 // always set new enemy to start at -150
       this.y = Math.floor(Math.random() * (475 - 350)) + 350 // generate random 'row' for enemy
       this.speed = Math.floor((Math.random() * 200) + 50) // generate random speed
@@ -49,18 +70,19 @@ class Paintbrush {
   }
 
   reset() {
-  this.x = 600
-  this.y = Math.floor(Math.random() * (350 - 200)) + 200
+    this.x = 600
+    this.y = Math.floor(Math.random() * (350 - 200)) + 200
   }
 }
 // Setting player constructor function
 class Player {
   constructor() {
-      this.x = 200
-      this.y = 625
-      this.score = 0
-      this.lives = 3
-      this.sprite = 'images/bob-ross.png'
+    this.x = 200
+    this.y = 625
+    this.score = 0
+    this.level = 1
+    this.lives = 3
+    this.sprite = 'images/bob-ross.png'
   }
 
   handleInput(inputKey) {
@@ -81,13 +103,12 @@ class Player {
   }
 
   update() {
-      checkCollision()
-      if (this.y < 30) {
-      this.score++
-      this.reset()
-      }
+    checkCollision()
+    if (this.y < 100) {
+    this.score++
+    this.reset()
+    }
   }
-
   // Reset function to set player back to starting position
   reset() {
     this.x = 200
@@ -106,19 +127,20 @@ checkCollision = () => {
           }
       }
       for (let i = 0; i < allPaintbrush.length; i++) {
-          if (Math.abs(player.x - allPaintbrush[i].x) <= 50) {
-              if (Math.abs(player.y - allPaintbrush[i].y) <= 50){
+          if (Math.abs(player.x - allPaintbrush[i].x) <= 100) {
+              if (Math.abs(player.y - allPaintbrush[i].y) <= 30){
                 player.lives--
                 player.reset()
-            }
+              }
           }
       }
   }
+
 // Place all enemy objects in an array called allEnemies
 var allEnemies = []
 // While there are fewer than 4 bugs, create new enemy and push into allEnemies
 while (allEnemies.length < 3) {
-  allEnemies.push(new Palette)
+  allEnemies.push(new Palette())
 }
 // Place the player object in a variable called player
 var allPaintbrush = []
@@ -140,111 +162,3 @@ document.addEventListener('keyup', e => {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-// // Enemies our player must avoid
-// var Enemy = function() {
-//     this.x = -150 // always set new enemy to start at -150
-//     this.y = Math.floor(Math.random() * 175) + 100 // generate random 'row' for enemy
-//     this.speed = Math.floor((Math.random() * 200) + 50) // generate random speed
-//     this.sprite = 'images/paint-palette.png'
-// };
-//
-// Enemy.prototype.update = function(dt) {
-//     // keeps the enemy moving until it crosses off screen, then resets position
-//     if(this.x <= 500) {
-//       this.x += this.speed * dt
-//     } else {
-//       this.reset()
-//     }
-// }
-// // Draw the enemy on the screen, required method for game
-// Enemy.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// }
-// // Reset enemy start position to fixed x value, random y value
-// Enemy.prototype.reset = function() {
-//   this.x = -100
-//   this.y = Math.floor(Math.random() * 175) + 150
-// }
-// // Setting player constructor function
-// var Player = function() {
-//     this.x = 200
-//     this.y = 475
-//     this.score = 0
-//     this.sprite = 'images/bob-ross.png'
-// }
-//
-// Player.prototype.handleInput = function(inputKey) {
-//
-//   if (inputKey === 'left' && this.x > 0) {
-//     this.x -= 100
-//   } else if (inputKey === 'right' && this.x < 400) {
-//     this.x += 100
-//   } else if (inputKey === 'down' && this.y < 450) {
-//     this.y += 100
-//   } else if (inputKey === 'up') {
-//       if (this.y < 0) {
-//         this.reset()
-//       } else {
-//       this.y -= 100
-//         }
-//   }
-// }
-//
-// Player.prototype.update = function() {
-//     checkCollision()
-//     if (this.y < 30) {
-//     this.score++
-//     this.reset()
-//     }
-//   }
-//
-// var checkCollision = function() {
-//       //Iterate through allEnemies array. Reset if enemy within 40px.
-//       var lives = 3
-//       for (var i = 0; i < allEnemies.length; i++) {
-//           if (Math.abs(player.x - allEnemies[i].x) <= 50) {
-//               if (Math.abs(player.y - allEnemies[i].y) <= 50) {
-//                   lives--
-//                   player.reset();
-//
-//               }
-//           }
-//       }
-//   }
-// // Reset function to set player back to starting position
-// Player.prototype.reset = function() {
-//   this.x = 200
-//   this.y = 475
-// }
-// // Render player on the screen
-// Player.prototype.render = function() {
-//   ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
-//   ctx.fillStyle = 'white'
-// 	ctx.clearRect(170,15,1000,35)
-//   ctx.font = "20px Verdana"
-//   ctx.fillStyle = 'red'
-//     ctx.fillText("Bob's Score: " + this.score,350, 45)
-//
-// }
-// // Now instantiate your objects.
-// // Place all enemy objects in an array called allEnemies
-// var allEnemies = []
-// // While there are fewer than 4 bugs, create new enemy and push into allEnemies
-// while (allEnemies.length < 4) {
-//   allEnemies.push(new Enemy)
-// }
-// // Place the player object in a variable called player
-// var player = new Player()
-// // This listens for key presses and sends the keys to your
-// // Player.handleInput() method. You don't need to modify this.
-// document.addEventListener('keyup', function(e) {
-//     var allowedKeys = {
-//         37: 'left',
-//         38: 'up',
-//         39: 'right',
-//         40: 'down'
-//     };
-//
-//     player.handleInput(allowedKeys[e.keyCode]);
-// });
--- frontend-nanodegree-arcade-game/js/engine.js
+++ frontend-nanodegree-arcade-game/js/engine.js
@@ -26,7 +26,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 505;
-    canvas.height = 606;
+    canvas.height = 808;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -120,14 +120,16 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
+                'images/water-block.png',
+                'images/water-block.png',  // Top row is water
+                'images/stone-block.png',
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
+            numRows = 8,
             numCols = 5,
             row, col;
 
